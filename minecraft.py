92568fbcb8966ad43950bfa616ac430079aa8748
Junus Ergin <junus91@googlemail.com>
2022-04-12 09:58:36 +0200

    added some mods

--- minecraft.py
+++ minecraft.py
@@ -4,35 +4,37 @@ app = Ursina()
 Sky()
 player = FirstPersonController()
 boxes = []
-for n in range(8):
-    for k in range(8):
-        box = Button(
-            parent=scene,
-            model='cube',
-            origin=0.5,
-            textute='white',
-            color=color.orange,
-            highligt=color.lime,
-            position=(k, 0, n)
-        )
-        boxes.append(box)
 
-    def input(key):
-        for box in boxes:
-            if box.hovered:
-                if key == 'left mouse down':
-                    newBox = Button(
-                        parent=scene,
-                        model='cube',
-                        origin=0.5,
-                        textute='white',
-                        color=color.orange,
-                        highligt=color.lime,
-                        position= box.position + mouse.normal
-                    )
-                    boxes.append(newBox)
-                if key == 'left mouse down':
-                    boxes.remove(box)
-                    destroy(box)
+def random_color():
+    red = random.Random().random() * 255
+    green = random.Random().random() * 255
+    blue = random.Random().random() * 255
+    return color.rgb(red, green, blue)
+
+def add_box(position):
+    box = Button(
+        parent=scene,
+        model='cube',
+        origin=0.5,
+        color= random_color(), #color.blue,
+        texture = 'grass', #https://stackoverflow.com/questions/66385821/how-to-make-python-ursina-engines-cube-texture
+        highligt=color.lime,
+        position=position
+    )
+    boxes.append(box)
+
+
+for x in range(20):
+    for y in range(20):
+        add_box(position=(x, 0, y))
+
+def input(key):
+    for box in boxes:
+        if box.hovered:
+            if key == 'left mouse down':
+                add_box(position=box.position + mouse.normal)
+            if key == 'right mouse down':
+                boxes.remove(box)
+                destroy(box)
 
 app.run()
